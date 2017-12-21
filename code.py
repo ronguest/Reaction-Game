3b029f06e0be388553d8989b993882d89ab8df82
roadfun <ronaldg@PowerHouse.local>
2017-12-21 09:12:28 -0600

    Basic game function working
    Needs difficulty adjustment to be added later

--- code.py
+++ code.py
@@ -3,6 +3,8 @@
 import board
 import neopixel
 import time
+import random
+from analogio import AnalogIn
 from adafruit_circuitplayground.express import cpx
 
 RED = (0x10, 0, 0) # 0x100000 also works
@@ -21,14 +23,45 @@ pixels.fill(BLACK)
 pixels.show()
 
 def game(delay):
-    for i in range(len(pixels)):
-        pixels[i] = BLACK
-        if i == len(pixels)-1:
-            pixels[0] = PURPLE
-        else:
-            pixels[i+1] = PURPLE
+    # Seed the random function with noise
+    a4 = AnalogIn(board.A4)
+    a5 = AnalogIn(board.A5)
+    a6 = AnalogIn(board.A6)
+    a7 = AnalogIn(board.A7)
+
+    seed  = a4.value
+    seed += a5.value
+    seed += a6.value
+    seed += a7.value
+
+    random.seed(seed)
+
+    # The LEDs are 1..10 from a player's perspective
+    # but 0..9 in terms of the pixels array index
+    target = random.randint(0, 9)
+    print("Player target =",target+1)
+    #time.sleep(5)
+
+    pixels.fill(BLACK)
+    while True:
+        for i in range(len(pixels)):
+            if i == target:
+                pixels[i] = RED
+            else:
+                pixels[i] = BLUE
+            if i == 0:
+                pixels[len(pixels)-1] = BLACK
+            else:
+                pixels[i-1] = BLACK
+
             time.sleep(delay)
-    won()
+            if cpx.touch_A5:
+                print("Player touched A5, i = ",i+1)
+                if i == target:
+                    won()
+                else:
+                    lost()
+                return
 
 def lost():
     pixels.fill(RED)
@@ -47,9 +80,9 @@ while True:
     while not cpx.button_b:
         # Wait until player pushes button B before starting, flash blue while waiting
         pixels.fill(BLUE)
-        time.sleep(.1)
+        time.sleep(.3)
         pixels.fill(BLACK)
         time.sleep(.1)
     # Give player 1 second to get ready for the game to start
     time.sleep(1)
-    game(.5)
+    game(.05)
